fd457422a3c744a644d941093f0f461213f4d78f
void <git.email.address@googlemail.com>
2010-08-06 16:53:51 +0200

    Retrieval of data works fine now it seems

--- harvest.rb
+++ harvest.rb
@@ -5,12 +5,6 @@ require 'nil/http'
 require 'SC2Stats'
 
 stats = SC2Stats.new('leagues')
-threads = []
 (1..12).each do |i|
-	thread = Thread.new { stats.processForumPage(i) }
-	threads << thread
-end
-
-threads.each do |thread|
-	thread.join
+	stats.processForumPage(i)
 end
